f26edf4b64b92ac84ee6c329574b35f3b0d0a6c7
evandroroncatto <ugrkvs@gmailcom>
2022-06-25 18:36:18 +0300

    first commit

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,8 +1,11 @@
 public class Example {
 
+    private static String x = "hahahaha";
+
     public static void main(String args[]){
 
         String aa  = " yoo foo doo";
+        System.out.println(x);
         
     }
 }
